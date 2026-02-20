8f024f417a0ec12877d1a4587464568a50743f9b
Ezra Maccabee <von.akimov@gmail.com>
2026-02-20 21:02:22 +0300

    Update palindrome_check.py

--- sprint_1/palindrome_check.py
+++ sprint_1/palindrome_check.py
@@ -6,13 +6,48 @@ def get_word():
     return word.lower()
 
 
+def is_word_palindrome(text: str):
+
+    text_length = len(text)
+    if text_length == 1:
+        print("To compare:", text[0], text[0])
+        if text[0].isalnum():
+            return True
+        else:
+            return False
+
+    left_symbol_ptr = 0
+    right_symbol_ptr = text_length - 1
+
+    print("Initial pointers:", text[left_symbol_ptr], text[right_symbol_ptr])
+
+    while left_symbol_ptr < right_symbol_ptr:
+        # Пропускаем все кроме цифр и букв
+        if not text[left_symbol_ptr].isalnum():
+            left_symbol_ptr += 1
+            continue
+        # Пропускаем все кроме цифр и букв
+        if not text[right_symbol_ptr].isalnum():
+            right_symbol_ptr -= 1
+            continue
+
+        print("To compare:", text[left_symbol_ptr], text[right_symbol_ptr])
+        # Сюда попадаем, тогда, когда оба указателя указывают на валидный символ
+        if text[left_symbol_ptr] != text[right_symbol_ptr]:
+            return False
+
+        # Перемещаем указатели на следующий символ
+        left_symbol_ptr += 1
+        right_symbol_ptr -= 1
+
+    return True
+
+
 if __name__ == "__main__":
     word = get_word()
+
     if word is not None:
-        reversed_word = word[::-1]
-        for char in word:
-            if char.isalnum():
-                print("YES")
-                break
+        if is_word_palindrome(word):
+            print("TRUE")
         else:
-            print("NO")
+            print("FALSE")
